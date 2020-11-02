91da4c0329fbe9adaf890a89b8010e3f423b86cc
Ivo Saavedra <ivo_saavedra@outlook.com>
2020-11-02 18:20:21 +0000

    added missing files

---
+++ lib/pages/EditQuestionPage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:inquirescape/Question.dart';
+
+class EditQuestionPage extends StatefulWidget {
+  final Question _question;
+  EditQuestionPage(this._question);
+
+  @override
+  _EditQuestionPage createState() => _EditQuestionPage();
+}
+
+class _EditQuestionPage extends State<EditQuestionPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Question'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+                keyboardType: TextInputType.multiline,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  contentPadding:
+                      new EdgeInsets.symmetric(vertical: 25.0, horizontal: 8),
+                ),
+                maxLines: null,
+                initialValue: widget._question.description,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Empty description.";
+                  }
+                },
+                onSaved: (String value) {
+                  widget._question.updateDescription(value);
+                  Navigator.pop(context);
+                }),
+          ],
+        ),
+      ),
+      persistentFooterButtons: [saveButton(context)],
+    );
+  }
+
+  Widget saveButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        // Validate will return true if the form is valid, or false if
+        // the form is invalid.
+        if (_formKey.currentState.validate()) {
+          _formKey.currentState.save();
+        }
+      },
+      child: Icon(Icons.save),
+    );
+  }
+}
--- lib/pages/QuestionFullPage.dart
+++ lib/pages/QuestionFullPage.dart
@@ -49,7 +49,7 @@ class _QuestionFullPageState extends State<QuestionFullPage> {
         Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (ctx) => EditQuestionPage(widget._question)))
+                builder: (ctx) => EditQuestionPage(widget._question))),
       },
     );
   }
