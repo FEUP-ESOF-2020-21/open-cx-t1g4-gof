697b3aee80f4b4411ec0a595a081744f86b408c0
Telmooo <telmoabaptista@hotmail.com>
2020-11-12 17:47:16 +0000

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:inquirescape/pages/QuestionListPage.dart';
 import 'package:inquirescape/widgets/QuestionsHolder.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
