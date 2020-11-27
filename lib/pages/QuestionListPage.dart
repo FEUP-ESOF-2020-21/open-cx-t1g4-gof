d57cf7caad5a484dd47aef76abd4a52b2059b8fa
Tiago Duarte Silva <tiagodusilva@gmail.com>
2020-11-27 11:17:48 +0000

    Added sorting to QuestionListPage Supports sort by date, rating, author and platform Supports toggle between ascending and descending order

--- lib/pages/QuestionListPage.dart
+++ lib/pages/QuestionListPage.dart
@@ -3,6 +3,9 @@ import 'package:inquirescape/firebase/FirebaseController.dart';
 import 'package:inquirescape/model/Question.dart';
 import 'package:inquirescape/widgets/QuestionCard.dart';
 
+// This is the type used by the popup menu below.
+enum _SortSetting { date, rating, author, platform }
+
 class QuestionListPage extends StatefulWidget {
   final FirebaseController _fbController;
   final Widget _drawer;
@@ -16,10 +19,23 @@ class QuestionListPage extends StatefulWidget {
 }
 
 class QuestionListPageState extends State<QuestionListPage> {
+  final List<PopupMenuEntry<_SortSetting>> sortButtonEntries = [];
+  _SortSetting _selection = _SortSetting.date;
+  bool descending = true;
+
   @override
   void initState() {
     this.widget._fbController.reloadQuestions((arg) {
       setState(() {});
+    });
+
+    _SortSetting.values.forEach((value) {
+      this.sortButtonEntries.add(
+        PopupMenuItem<_SortSetting>(
+          value: value,
+          child: Text(this._getSortText(value)),
+        ),
+      );
     });
 
     super.initState();
@@ -28,6 +44,38 @@ class QuestionListPageState extends State<QuestionListPage> {
   @override
   Widget build(BuildContext context) {
     List<Question> questions = this.widget._fbController.conferenceQuestions;
+    if (questions != null) {
+      switch (this._selection) {
+        case _SortSetting.date:
+          if (this.descending)
+            questions.sort((a, b) => a.postDate.compareTo(b.postDate));
+          else
+            questions.sort((b, a) => a.postDate.compareTo(b.postDate));
+          break;
+        case _SortSetting.rating:
+          if (this.descending)
+            questions.sort((a, b) => a.avgRating.compareTo(b.avgRating));
+          else
+            questions.sort((b, a) => a.avgRating.compareTo(b.avgRating));
+          break;
+        case _SortSetting.author:
+          if (this.descending)
+            questions.sort((a, b) =>
+                a.authorDisplayName.compareTo(b.authorDisplayName));
+          else
+            questions.sort((b, a) =>
+                a.authorDisplayName.compareTo(b.authorDisplayName));
+          break;
+        case _SortSetting.platform:
+          if (this.descending)
+            questions.sort((a, b) =>
+                a.authorPlatform.compareTo(b.authorPlatform));
+          else
+            questions.sort((b, a) =>
+                a.authorPlatform.compareTo(b.authorPlatform));
+          break;
+      }
+    }
 
     return SafeArea(
       child: RefreshIndicator(
@@ -37,7 +85,9 @@ class QuestionListPageState extends State<QuestionListPage> {
             centerTitle: true,
           ),
           drawer: this.widget._drawer,
-          body: questions == null ? _questionsUnloaded(context) : _questionList(context, questions),
+          body: questions == null
+              ? _questionsUnloaded(context)
+              : _questionList(context, questions),
         ),
         onRefresh: this._onRefresh,
       ),
@@ -58,18 +108,80 @@ class QuestionListPageState extends State<QuestionListPage> {
     print("I feel rather refreshed");
   }
 
+  Widget _sortButton(BuildContext context) {
+    return PopupMenuButton<_SortSetting>(
+        icon: Icon(
+          Icons.filter_alt_outlined,
+          color: Colors.grey,
+        ),
+        onSelected: (_SortSetting result) {
+          setState(() {
+            _selection = result;
+          });
+        },
+        itemBuilder: (ctx) => this.sortButtonEntries,
+    );
+  }
+
+  String _getSortText(_SortSetting sortSet) {
+    switch (sortSet) {
+      case _SortSetting.date:
+        return "Date";
+        break;
+      case _SortSetting.rating:
+        return "Rating";
+        break;
+      case _SortSetting.author:
+        return "Author";
+        break;
+      case _SortSetting.platform:
+        return "Platform";
+        break;
+    }
+  }
+
   Widget _questionList(BuildContext context, List<Question> questions) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: questions.length,
-      itemBuilder: (BuildContext context, int index) {
-        return QuestionCard(
-          question: questions[index],
-          questionIndex: index,
-          fbController: this.widget._fbController,
-          onUpdate: () => this.setState(() {}),
-        );
-      },
+    return Column(
+      children: [
+        Align(
+          child: Row(
+            children: [
+              this._sortButton(context),
+              IconButton(
+                icon: Icon(
+                  this.descending ? Icons.arrow_downward : Icons.arrow_upward,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    this.descending = !this.descending;
+                  });
+                },
+              ),
+              Text(
+                "Sorting by: " + this._getSortText(this._selection),
+                style: TextStyle(fontSize: 16),
+                overflow: TextOverflow.clip,
+              ),
+            ],
+          ),
+          alignment: Alignment.centerLeft,
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: questions.length,
+            itemBuilder: (BuildContext context, int index) {
+              return QuestionCard(
+                question: questions[index],
+                questionIndex: index,
+                fbController: this.widget._fbController,
+                onUpdate: () => this.setState(() {}),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
