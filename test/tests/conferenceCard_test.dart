7c1c023538143f7245dede4107f4c8e23242406f
ivSaav <ivo_saavedra@outlook.com>
2020-12-20 20:12:26 +0000

    reorganized tests folder

--- test/tests/conferenceCard_test.dart
+++ test/tests/conferenceCard_test.dart
@@ -6,28 +6,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:inquirescape/widgets/tags/Tag.dart';
 import 'package:mockito/mockito.dart';
 
-
 // Mock class
-class MockDocumentReference extends Mock implements DocumentReference {
-
-}
+class MockDocumentReference extends Mock implements DocumentReference {}
 
 void main() {
-
   Conference c;
 
   setUp(() {
     // Create mock object.
-    c = Conference.withoutRef("talk title", "description", "speaker", DateTime.parse("2020-12-03 08:30"), ["topic1", "topic2", "topic3"]);
+    c = Conference.withoutRef(
+        "talk title", "description", "speaker", DateTime.parse("2020-12-03 08:30"), ["topic1", "topic2", "topic3"]);
     DocumentReference mock = MockDocumentReference();
     when(mock.id).thenReturn("abcdefg");
     c.docRef = mock;
   });
 
-
   testWidgets("Test title", (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-        home: Scaffold(body: ConferenceCard(conference: c,))));
+        home: Scaffold(
+            body: ConferenceCard(
+      conference: c,
+    ))));
 
     // Test Conference title
     final titleFinder = find.text("talk title");
@@ -36,7 +35,10 @@ void main() {
 
   testWidgets("Test tag number", (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-        home: Scaffold(body: ConferenceCard(conference: c,))));
+        home: Scaffold(
+            body: ConferenceCard(
+      conference: c,
+    ))));
 
     // Test number of tags
     final tagFinder = find.byType(Tag);
@@ -45,11 +47,13 @@ void main() {
 
   testWidgets("Test date", (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-        home: Scaffold(body: ConferenceCard(conference: c,))));
+        home: Scaffold(
+            body: ConferenceCard(
+      conference: c,
+    ))));
 
     // Test date
     final titleFinder = find.text("03-12-2020 08:30");
     expect(titleFinder, findsOneWidget);
   });
-
 }
